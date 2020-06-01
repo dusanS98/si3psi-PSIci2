028e4c49150f8a6f3e58f77cf9fc8a0af88a2c5d
dusanS98 <61711060+dusanS98@users.noreply.github.com>
2020-06-01 23:31:17 +0200

    Popunjen skript

--- Baza/Faza4/pethotelPopunjen.sql
+++ Baza/Faza4/pethotelPopunjen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 01, 2020 at 02:14 PM
+-- Generation Time: Jun 01, 2020 at 09:28 PM
 -- Server version: 8.0.18
 -- PHP Version: 7.3.12
 
@@ -175,6 +175,13 @@ CREATE TABLE IF NOT EXISTS `reservationpet` (
 --
 
 INSERT INTO `reservationpet` (`username`, `petId`, `dateTime`) VALUES
+('eucc', 1, '2020-06-01 06:00:00'),
+('eucc', 2, '2020-06-01 07:00:00'),
+('eucc', 3, '2020-06-01 08:00:00'),
+('eucc', 4, '2020-06-01 09:00:00'),
+('mitztetrebic', 2, '2020-06-02 06:00:00'),
+('mitztetrebic', 6, '2020-06-01 10:00:00'),
+('mitztetrebic', 9, '2020-06-26 06:00:00'),
 ('user123', 2, '2020-06-01 12:00:00'),
 ('user123', 6, '2020-06-12 08:00:00');
 
@@ -263,12 +270,21 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`username`, `password`, `firstName`, `lastName`, `email`, `phone`, `type`) VALUES
 ('admin', 'Admin$1234', 'Admin', 'Admin', 'admin@mail.com', '01234567', 'admin'),
+('asdasd', 'mmmmmmm', 'Milica', 'Kaitovic', 'mmm@gmail.m', '0643974584', 'standard'),
+('bla', 'kameleon', 'mala', 'maca', 'mkaitovic@gmail.commm', '0643756544', 'standard'),
+('djuriceva', 'mrzimmacke', 'Ana', 'Djuric', 'jamrzimmacke@gmail.com', '0645558884', 'standard'),
 ('dusan', 'dusan123', 'Dušan', 'Stanivuković', 'dusan@mail.com', '01234567', 'admin'),
+('eucc', 'misica', 'Nikola', 'Antonic', 'misica123@gmail.com', '062778899', 'standard'),
+('greska', '$2y$10$Jbd8ui7xQ', 'Katarina', 'Grujic', 'kg@gmail.com', '064444444', 'standard'),
 ('jovan', 'jovan123', 'Jovan', 'Penezić', 'jovan@mail.com', '01234567', 'admin'),
+('mic', 'micmic', 'Milica', 'ASDasd', 'mkasm@gmail.com', '098675843', 'standard'),
 ('milicaJ', 'milicaj123', 'Milica', 'Janković', 'milicaj@mail.com', '01234567', 'admin'),
 ('milicaK', 'milicak123', 'Milica', 'Kaitović', 'milicak@mail.com', '01234567', 'admin'),
+('mitztetrebic', 'micatrepavica', 'Milica', 'Kaitovic', 'mkaitovic@gmail.com', '0643434345', 'standard'),
 ('moderator123', 'sifra123', 'Marko', 'Marković', 'marko@gmail.com', '239168', 'moderator'),
 ('pera', 'sifra123', 'Petar', 'Petrović', 'petar.petrovic@gmail.com', '0602498321', 'standard'),
+('slonica', 'flasica', 'Milica', 'Kaitovic', 'km@etf.bg.rs', '0645554443', 'standard'),
+('takomlada', 'kobalada', 'Tanja', 'Savic', 'tanja@gmail.vom', '0645558884', 'standard'),
 ('user123', 'sifra123', 'Pera', 'Perić', 'pera@gmail.com', '123412', 'standard');
 
 -- --------------------------------------------------------
